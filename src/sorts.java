24e8c5f748eeda9d763c965eed2a916afd4280f9
andreabarrera <andreabarrera@192.168.0.10>
2014-07-24 07:30:40 -0600

    2 commit

--- src/sorts.java
+++ src/sorts.java
@@ -1,12 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author andreabarrera
- */
 public class sorts {
     private static void merge(int data[], int temp[],int low, int middle, int high)
     // pre: data[middle..high] are ascending
