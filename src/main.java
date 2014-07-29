8855ee7deb9030249fb21e9e1c314066c92f4dfc
andreabarrera <andreabarrera@192.168.0.10>
2014-07-29 14:00:07 -0600

    6 commit

--- src/main.java
+++ src/main.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class main {
     public static void main(String[] args){
         Grandom lisdes = new Grandom();
-        MergeSort lista = new MergeSort(lisdes.generarRandom());
-        System.out.println(Arrays.toString(lista.getNumbers()));
+        BubbleSort lista = new BubbleSort(lisdes.generarRandom());
+        System.out.println(Arrays.toString(lista.getBubbleSort()));
     }
     
 }
